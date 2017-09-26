532e1675faf36c046a1178b70c15a7a93c9682c9
纪欢 <jk_g@163.com>
2017-09-27 00:22:37 +0800

    Hello World

---
+++ jihuan/src/main/java/com/algorithm/TestHello.java
@@ -0,0 +1,14 @@
+package com.algorithm;
+/**
+ * Title: 类的主题（开发人员填写）
+ * Description: 该类的功能
+ *
+ * @author jihuan
+ * @create 2017-09-27 00:21
+ * @version: 1.0
+ */
+public class TestHello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
